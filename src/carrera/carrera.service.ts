bfaf5e8e62460c2c5288056afac1152646c62b92
Anthonyx578 <antrogartme2002@gmail.com>
2024-12-13 20:55:18 -0500

    cambios en el orden del get

--- src/carrera/carrera.service.ts
+++ src/carrera/carrera.service.ts
@@ -8,84 +8,107 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class CarreraService {
-    constructor(@InjectRepository(carrera) private repositoty:Repository<carrera>){
+  constructor(
+    @InjectRepository(carrera) private repositoty: Repository<carrera>,
+  ) {}
+
+  async Create(Carrera: carreraDTO) {
+    try {
+      const NewCarrera: Partial<carrera> = {
+        nombre_carrera: Carrera.nombre_carrera,
+        created_at: new Date(),
+      };
+      return await this.repositoty.save(NewCarrera);
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
 
-    async Create (Carrera:carreraDTO){
-        try{
-            const NewCarrera:Partial<carrera> = {
-                nombre_carrera: Carrera.nombre_carrera,
-                created_at: new Date()
-            }
-            return await this.repositoty.save(NewCarrera);
+  async GetAll(Pagination: PaginationDto) {
+    try {
+      const { page, limit } = Pagination;
+
+      const TotalData = await this.repositoty.count({
+        where: { status: 1 },
+      });
+      const TotalPages = Math.ceil(TotalData / limit);
+
+      const data = await this.repositoty.find({
+        select: ['id_carrera', 'nombre_carrera'],
+        skip: (page - 1) * limit,
+        take: limit,
+        order:{
+            id_carrera:'DESC'
         }
-        catch(e){
-            throw new RpcException(e)
-        }
+      });
+
+      return {
+        data,
+        meta: { TotalPages: TotalPages, CurrentPage: page, DataCount: limit },
+      };
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
 
-    async GetAll(Pagination:PaginationDto){
-       try{
-            const {page,limit} = Pagination;
-
-            const TotalData = await this.repositoty.count({
-                where:{status:1},
-            })
-            const TotalPages =Math.ceil(TotalData/limit);
-
-            const data = await this.repositoty.find({select:['id_carrera','nombre_carrera'],skip:((page - 1) * limit),take:limit})
-
-            return {data,meta:{TotalPages:TotalPages,CurrentPage:page,DataCount:limit}}
-       } 
-       catch(e){
-        throw new RpcException(e)
-        }
+  async Get(id: number) {
+    try {
+      return await this.repositoty.findOne({
+        where: { id_carrera: id, status: 1 },
+        select: ['id_carrera', 'nombre_carrera'],
+      });
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
+  async update(id: number, ChangeData: carreraDTO) {
+    try {
+      const ExistData = await this.repositoty.findOne({
+        where: { id_carrera: id, status: 1 },
+      });
+      if (!ExistData) {
+        return {};
+      }
+      return this.repositoty.update(
+        { id_carrera: id },
+        { ...ChangeData, updated_at: new Date() },
+      );
+    } catch (e) {
+      throw new RpcException(e);
+    }
+  }
 
-    async Get (id:number){
-        try{
-            return await this.repositoty.findOne({where:{id_carrera:id,status:1},select:['id_carrera','nombre_carrera']})
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
+  async delete(id) {
+    try {
+      const Exist = this.repositoty.findOne({
+        where: { id_carrera: id, status: 1 },
+      });
+      if (!Exist) {
+        return { message: 'Dato no encontrado o ya eliminado' };
+      }
+      return this.repositoty.update(
+        { id_carrera: id },
+        { deleted_at: new Date(), status: 0 },
+      );
+    } catch (e) {
+      throw new RpcException(e);
     }
-    async update (id:number,ChangeData:carreraDTO){
-        try{
-            const ExistData = await this.repositoty.findOne({where:{id_carrera:id,status:1}})
-            if(!ExistData){
-                return {}
-            }
-            return this.repositoty.update({ id_carrera: id }, { ...ChangeData, updated_at: new Date() });
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
-    }
+  }
 
-    async delete (id){
-        try{
-            const Exist = this.repositoty.findOne({where:{id_carrera:id,status:1}})
-            if(!Exist){
-                return {message:"Dato no encontrado o ya eliminado"}
-            }
-            return this.repositoty.update({id_carrera:id},{deleted_at:new Date(),status:0})
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
+  async restore(id) {
+    try {
+      const Exist = this.repositoty.findOne({
+        where: { id_carrera: id, status: 0 },
+      });
+      if (!Exist) {
+        return { message: 'Dato no encontrado o ya restaurado' };
+      }
+      return this.repositoty.update(
+        { id_carrera: id },
+        { deleted_at: null, status: 1 },
+      );
+    } catch (e) {
+      throw new RpcException(e);
     }
-
-    async restore (id){
-        try{
-            const Exist = this.repositoty.findOne({where:{id_carrera:id,status:0}})
-            if(!Exist){
-                return {message:"Dato no encontrado o ya restaurado"}
-            }
-            return this.repositoty.update({id_carrera:id},{deleted_at:null,status:1})
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
-    }
+  }
 }
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -9,111 +9,167 @@ import { UsuarioUpdateDTO } from 'src/entitys/DTO/usuario.Update.DTO';
 
 @Injectable()
 export class UsuarioService {
-    constructor(@InjectRepository(usuario) private repository:Repository<usuario>){
+  constructor(
+    @InjectRepository(usuario) private repository: Repository<usuario>,
+  ) {}
+
+  async Create(Usuario: UsuarioUpdateDTO) {
+    try {
+      const { contrasena, ...OtherData } = Usuario;
+      const hashedCont = await this.hashPassword(contrasena);
+      const NewCarrera = {
+        ...OtherData,
+        contrasena: hashedCont,
+        created_at: new Date(),
+      };
+      return await this.repository.save(NewCarrera);
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
 
-    async Create (Usuario:UsuarioUpdateDTO){
-        try{
-            const {contrasena, ...OtherData} = Usuario
-            const hashedCont = await this.hashPassword(contrasena)
-            const NewCarrera = {
-                ...OtherData,
-                contrasena: hashedCont,
-                created_at:new Date()
-            }
-            return await this.repository.save(NewCarrera);
+  async GetAll(Pagination: PaginationDto) {
+    try {
+      const { page, limit } = Pagination;
+
+      const TotalData = await this.repository.count({
+        where: { status: 1 },
+      });
+      const TotalPages = Math.ceil(TotalData / limit);
+
+      const data = await this.repository.find({
+        where: { status: 1 },
+        select: [
+          'id_usuario',
+          'nombre_usuario',
+          'nombres',
+          'apellidos',
+          'telefono',
+          'correo',
+          'fecha_nacimiento',
+          'id_rol',
+          'id_carrera',
+        ],
+        skip: (page - 1) * limit,
+        take: limit,
+        order:{
+            id_usuario:'DESC'
         }
-        catch(e){
-            throw new RpcException(e)
-        }
-    }
-
-    async GetAll(Pagination:PaginationDto){
-       try{
-            const {page,limit} = Pagination;
-
-            const TotalData = await this.repository.count({
-                where:{status:1},
-            })
-            const TotalPages =Math.ceil(TotalData/limit);
-
-            const data = await this.repository.find({where:{status:1},select:['id_usuario','nombre_usuario','nombres','apellidos','telefono','correo','fecha_nacimiento','id_rol','id_carrera'],skip:((page - 1) * limit),take:limit})
-            /*
+      });
+      /*
             data.map((data)=>{
                 data.id_rol
             })
             */
-            return {data,meta:{TotalPages:TotalPages,CurrentPage:page,DataCount:limit}}
-       } 
-       catch(e){
-        throw new RpcException(e)
-        }
+      return {
+        data,
+        meta: { TotalPages: TotalPages, CurrentPage: page, DataCount: limit },
+      };
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
 
-    async Get (id:number){
-        try{
-            const Find = await this.repository.findOne({where:{id_usuario:id,status:1},select:['id_usuario','nombre_usuario','nombres','apellidos','telefono','correo','fecha_nacimiento','id_rol','id_carrera']})
-            if(!Find){
-                return null;
-            }
-            return Find;
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
+  async Get(id: number) {
+    try {
+      const Find = await this.repository.findOne({
+        where: { id_usuario: id, status: 1 },
+        select: [
+          'id_usuario',
+          'nombre_usuario',
+          'nombres',
+          'apellidos',
+          'telefono',
+          'correo',
+          'fecha_nacimiento',
+          'id_rol',
+          'id_carrera',
+        ],
+      });
+      if (!Find) {
+        return null;
+      }
+      return Find;
+    } catch (e) {
+      throw new RpcException(e);
     }
-    async update (id:number,ChangeData:UsuarioUpdateDTO){
-        try{
-            const ExistData = await this.repository.findOne({where:{id_usuario:id,status:1}})
-            if(!ExistData){
-                return {}
-            }
-            return await this.repository.update({ id_usuario: id }, { ...ChangeData, updated_at: new Date() });
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
-    }A
-
-    async delete (id){
-        try{
-            const Exist = this.repository.findOne({where:{id_usuario:id,status:1}})
-            if(!Exist){
-                return {message:"Dato no encontrado o ya eliminado"}
-            }
-            return this.repository.update({id_usuario:id},{deleted_at:new Date(),status:0})
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
+  }
+  async update(id: number, ChangeData: UsuarioUpdateDTO) {
+    try {
+      const ExistData = await this.repository.findOne({
+        where: { id_usuario: id, status: 1 },
+      });
+      if (!ExistData) {
+        return {};
+      }
+      return await this.repository.update(
+        { id_usuario: id },
+        { ...ChangeData, updated_at: new Date() },
+      );
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
+  A;
 
-    async restore (id){
-        try{
-            const Exist = this.repository.findOne({where:{id_usuario:id,status:0}})
-            if(!Exist){
-                return {message:"Dato no encontrado o ya restaurado"}
-            }
-            return this.repository.update({id_usuario:id},{deleted_at:null,status:1})
-        }
-        catch(e){
-            throw new RpcException(e)
-        }
+  async delete(id) {
+    try {
+      const Exist = this.repository.findOne({
+        where: { id_usuario: id, status: 1 },
+      });
+      if (!Exist) {
+        return { message: 'Dato no encontrado o ya eliminado' };
+      }
+      return this.repository.update(
+        { id_usuario: id },
+        { deleted_at: new Date(), status: 0 },
+      );
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
 
-    async FindbyUser (nombre_usuario:string){
-        return await this.repository.findOne({where:{nombre_usuario:nombre_usuario,status:1},select:['id_usuario','nombre_usuario','contrasena','id_carrera','id_rol']})
+  async restore(id) {
+    try {
+      const Exist = this.repository.findOne({
+        where: { id_usuario: id, status: 0 },
+      });
+      if (!Exist) {
+        return { message: 'Dato no encontrado o ya restaurado' };
+      }
+      return this.repository.update(
+        { id_usuario: id },
+        { deleted_at: null, status: 1 },
+      );
+    } catch (e) {
+      throw new RpcException(e);
     }
+  }
 
+  async FindbyUser(nombre_usuario: string) {
+    return await this.repository.findOne({
+      where: { nombre_usuario: nombre_usuario, status: 1 },
+      select: [
+        'id_usuario',
+        'nombre_usuario',
+        'contrasena',
+        'id_carrera',
+        'id_rol',
+      ],
+    });
+  }
 
-    //Funciones Adicionales
-    async hashPassword(password: string): Promise<string> {
-        const saltRounds = 10; // Número de rondas de salt para el hashing
-        const salt = await bcrypt.genSalt(saltRounds);
-        return await bcrypt.hash(password, salt);
-    }
+  //Funciones Adicionales
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10; // Número de rondas de salt para el hashing
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(password, salt);
+  }
 
-    async comparePasswords(plainPassword: string, hashedPassword: string): Promise<boolean> {
-        return await bcrypt.compare(plainPassword, hashedPassword);
-    }
+  async comparePasswords(
+    plainPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(plainPassword, hashedPassword);
+  }
 }
