b8e00d57efb4cb8698a2102ed646225156d139a2
Anthonyx578 <antrogartme2002@gmail.com>
2024-12-19 18:05:35 -0500

    mapeo

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -3,11 +3,15 @@ import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { usuario } from 'src/entitys/usuario.entity';
+import { RolService } from 'src/rol/rol.service';
+import { CarreraService } from 'src/carrera/carrera.service';
+import { rol } from 'src/entitys/rol.entity';
+import { carrera } from 'src/entitys/carrera.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([usuario])],
+  imports:[TypeOrmModule.forFeature([usuario,rol,carrera])],
   controllers: [UsuarioController],
-  providers: [UsuarioService],
+  providers: [UsuarioService,RolService,CarreraService],
   exports:[UsuarioService]
 })
 export class UsuarioModule {}
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -7,11 +7,15 @@ import { ILike, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { UsuarioUpdateDTO } from 'src/entitys/DTO/usuario.Update.DTO';
 import { rol } from 'src/entitys/rol.entity';
+import { RolService } from 'src/rol/rol.service';
+import { CarreraService } from 'src/carrera/carrera.service';
 
 @Injectable()
 export class UsuarioService {
   constructor(
     @InjectRepository(usuario) private repository: Repository<usuario>,
+    private readonly rolService: RolService,
+    private readonly carreraService: CarreraService,
   ) {}
 
   async Create(Usuario: UsuarioUpdateDTO) {
@@ -50,13 +54,13 @@ export class UsuarioService {
           'fecha_nacimiento',
           'id_rol',
           'id_carrera',
-          'status'
+          'status',
         ],
         skip: (page - 1) * limit,
         take: limit,
-        order:{
-            id_usuario:'DESC'
-        }
+        order: {
+          id_usuario: 'DESC',
+        },
       });
       /*
             data.map((data)=>{
@@ -72,17 +76,28 @@ export class UsuarioService {
     }
   }
 
-  async GetAllByRol(Pagination: PaginationDto, Rol:number,searchLike:string) {
+  async GetAllByRol(
+    Pagination: PaginationDto,
+    Rol: number,
+    searchLike: string,
+  ) {
     try {
       const { page, limit } = Pagination;
-
       const TotalData = await this.repository.count({
-        where: { status: 1,id_rol:Rol},
+        where: { status: 1, id_rol: Rol },
       });
       const TotalPages = Math.ceil(TotalData / limit);
-      console.log(Rol)
+
       const data = await this.repository.find({
-        where: { status: 1 , id_rol:Rol,nombre_usuario:ILike(`%${searchLike}%`),nombres:ILike(`%${searchLike}%`),apellidos:ILike(`%${searchLike}%`),correo:ILike(`%${searchLike}%`)},
+        where: {
+          status: 1,
+          id_rol: Rol,
+          //Hacer Pruebas, porque no da datos si pongo mas de uno
+          nombre_usuario: ILike(`%${searchLike}%`),
+          /*nombres: ILike(`%${searchLike}%`),
+          apellidos: ILike(`%${searchLike}%`),
+          correo: ILike(`%${searchLike}%`),*/
+        },
         select: [
           'id_usuario',
           'nombre_usuario',
@@ -93,32 +108,44 @@ export class UsuarioService {
           'fecha_nacimiento',
           'id_rol',
           'id_carrera',
-          'status'
+          'status',
         ],
         skip: (page - 1) * limit,
         take: limit,
-        order:{
-            id_usuario:'DESC'
-        }
+        order: {
+          id_usuario: 'DESC',
+        },
       });
-      /*
-            data.map((data)=>{
-                data.id_rol
-            })
-            */
+      const MappedData = await this.MappearProfesor(data);
       return {
-        data,
-        meta: { TotalPages: TotalPages, CurrentPage: page, DataCount: limit,TotalData:TotalData },
+        Data: MappedData,
+        meta: {
+          TotalPages: TotalPages,
+          CurrentPage: page,
+          DataCount: limit,
+          TotalData: TotalData,
+        },
       };
     } catch (e) {
       throw new RpcException(e);
     }
   }
 
+  async MappearProfesor(data:usuario[]){
+    const MappedData = await Promise.all(
+      data.map(async (data) => {
+        const rolData = await this.rolService.Get(data.id_rol);
+        const CarreraData = await this.carreraService.Get(data.id_carrera);
+        return { ...data, ...rolData, ...CarreraData };
+      }),
+    );
+    return MappedData
+  }
+  
   async Get(id: number) {
     try {
       const Find = await this.repository.findOne({
-        where: { id_usuario: id},
+        where: { id_usuario: id },
         select: [
           'id_usuario',
           'nombre_usuario',
