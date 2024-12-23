ceb2e32b0410d6da9b645777d3e1840504374acf
Anthonyx578 <antrogartme2002@gmail.com>
2024-12-22 21:45:22 -0500

    cambio

--- src/requisito-cumplido/requisito-cumplido.module.ts
+++ src/requisito-cumplido/requisito-cumplido.module.ts
@@ -4,9 +4,10 @@ import { RequisitoCumplidoService } from './requisito-cumplido.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { requisitoCumplido } from './Entitys/requisito-cumplido.entity';
 import { RequisitoService } from 'src/requisito/requisito.service';
+import { requisito } from 'src/requisito/Entitys/requisitos.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([requisitoCumplido])],
+  imports:[TypeOrmModule.forFeature([requisitoCumplido,requisito])],
   controllers: [RequisitoCumplidoController],
   providers: [RequisitoCumplidoService,RequisitoService]
 })
