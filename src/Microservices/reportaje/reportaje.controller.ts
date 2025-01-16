054e30bf45dfa90ed8e974af567bb1c43fcc3709
Anthonyx578 <antrogartme2002@gmail.com>
2025-01-15 20:15:35 -0500

    cambios

--- src/Microservices/reportaje/reportaje.controller.ts
+++ src/Microservices/reportaje/reportaje.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiTags } from '@nestjs/swagger';
-import { first, firstValueFrom } from 'rxjs';
+import { first, firstValueFrom, map } from 'rxjs';
 import { ExeptValidator } from 'src/ExceptionValidator/ExceptionValidator';
 import { FailResponse } from 'src/Response/Responses';
 
@@ -16,36 +16,87 @@ export class ReportajeController {
       const EstudiantesIDs: any[] = await firstValueFrom(
         this.client.send({ cmd: 'GetAllEstudianteIDs' }, {}),
       );
+
       const NombresEstudiantes: any[] = await Promise.all(
         EstudiantesIDs.map(async (estudiantesIDs) => {
-          const nombres: { apellidos: string; nombres: string } =
-            await firstValueFrom(
-              this.client.send(
-                { cmd: 'GetUsuarioNames' },
-                estudiantesIDs.id_usuario,
-              ),
-            );
-          return { nombres: `${nombres.apellidos} ${nombres.apellidos}` };
-        }),
-      );
-      console.log(EstudiantesIDs);
-      const IDEstudiantes = EstudiantesIDs.map((ids) => ids.id_usuario);
-
-
-      const Requisitos = await Promise.all(
-        IDEstudiantes.map(async (id) => {
-          const Req = await firstValueFrom(
-            await this.client.send(
-              { cmd: 'GetAllByEstudianteRequisitoCumplido' },
-              IDEstudiantes,
+          const nombres: {
+            apellidos: string;
+            nombres: string;
+            id_carrera: number;
+          } = await firstValueFrom(
+            this.client.send(
+              { cmd: 'GetUsuarioNames' },
+              estudiantesIDs.id_usuario,
             ),
           );
-
-          return Req
+          console.log(nombres);
+          const Carrera = await firstValueFrom(
+            this.client.send({ cmd: 'GetCarrera' }, nombres.id_carrera),
+          );
+          console.log(Carrera);
+          return {
+            nombres: `${nombres.apellidos} ${nombres.apellidos}`,
+            Carrera: Carrera.nombre_carrera,
+          };
         }),
       );
-      
-      return NombresEstudiantes;
+
+      const IdRequisitos: any[] = await Promise.all(
+        EstudiantesIDs.map(async (estudiantesIDS) => {
+          const requisitos = await firstValueFrom(
+            this.client.send(
+              { cmd: 'GetRequisitoCumplidoEstudiante' },
+              estudiantesIDS.id_estudiante,
+            ),
+          );
+          return requisitos;
+        }),
+      );
+
+      const RequisitosCumplidos = await Promise.all(
+        EstudiantesIDs.map(async (estudiantes) => {
+          const RequisitosCumplidos = await firstValueFrom(
+            this.client.send({ cmd: 'ContarRequisito' }, estudiantes.id_estudiante),
+          );
+          return RequisitosCumplidos
+        }),
+      );
+      const RequisitosNombre = await Promise.all(
+        IdRequisitos.map(async (idrequisitos) => {
+          console.log(idrequisitos);
+          const Requisito = await Promise.all(
+            idrequisitos.map(async (ids) => {
+              const Requisitos = await firstValueFrom(
+                this.client.send({ cmd: 'GetRequisito' }, ids.id_requisito),
+              );
+              return { Requisito: Requisitos.documento };
+            }),
+          );
+          return Requisito;
+        }),
+      );
+
+      const RequisitosCombinados = IdRequisitos.map((requisitos, index) => {
+        const requisitosNombre = RequisitosNombre[index] || [];
+        const combinados = requisitosNombre.map((req) => {
+          const estado =
+            requisitos.find((r) => r.id_requisito === req.id_requisito)
+              ?.estado || 0;
+          return { ...req, estado };
+        });
+        return combinados;
+      });
+
+      const Mapeo: any[] = NombresEstudiantes.map((nombre, index) => {
+        return {
+          Nombres: nombre.nombres,
+          Carrera: nombre.Carrera,
+          RequistosCumplidos:RequisitosCumplidos[index],
+          Requisitos: RequisitosCombinados[index],
+        };
+      });
+
+      return Mapeo;
     } catch (error) {
       return FailResponse(error);
     }
